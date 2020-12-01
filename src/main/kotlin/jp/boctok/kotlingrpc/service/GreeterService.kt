54f542fb886d041484a7618e10882ab47028e787
M.Higashijima <boctok.ctpoba@gmail.com>
2020-12-02 02:10:46 +0900

    データ型変更

--- src/main/kotlin/jp/boctok/kotlingrpc/service/GreeterService.kt
+++ src/main/kotlin/jp/boctok/kotlingrpc/service/GreeterService.kt
@@ -8,7 +8,10 @@ import io.grpc.stub.StreamObserver
 @GRpcService
 class GreeterService: GreeterGrpc.GreeterImplBase() {
     override fun sayHello(request: GreeterOuterClass.HelloRequest, responseObserver: StreamObserver<GreeterOuterClass.HelloReply>){
-        val replyBuilder = GreeterOuterClass.HelloReply.newBuilder().setMessage("Hello " + request.name)
+        var name = if("".equals(request.name)) "blank name" else if(request.name==null) "no name" else request.name
+//        val replyBuilder = GreeterOuterClass.HelloReply.newBuilder().setMessage("Hello " + name)
+        var age = if("".equals(request.age)) "blank name" else if(request.age==null) "no name" else request.age
+        val replyBuilder = GreeterOuterClass.HelloReply.newBuilder().setMessage("Hello " + name + "/you're " + age)
         responseObserver.onNext(replyBuilder.build())
         responseObserver.onCompleted()
     }
